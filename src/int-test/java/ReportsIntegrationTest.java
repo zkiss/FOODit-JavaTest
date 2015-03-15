1583886e40f408eafde95867bec18f77f5c7631d
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2015-03-15 19:58:26 +0000

    Integration test stub

---
+++ src/int-test/java/ReportsIntegrationTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+public class ReportsIntegrationTest {
+
+	/*
+	 * Should do full integration test here.
+	 * 
+	 * Fire up local dev server
+	 * Load data
+	 * 
+	 * Send HTTP requests to the endpoints
+	 * Check data response
+	 * 
+	 * Shut down local dev server
+	 * 
+	 * I can see thundr-http-ning and appengine-testing is included probably for this reason. I have no more time to do
+	 * this.
+	 * 
+	 * An other problem I have with this is that there is no way to set up the data - the menu and order jsons are
+	 * bundled with the app. In order to be able to test this properly I'd have to create menu and order editing
+	 * endpoints. Just for testing.
+	 * 
+	 * If the app server is run in embedded mode then it would be fine as test resources would be picked up - same way
+	 * as in the Service test I've written. I have no idea if this is the case.
+	 * 
+	 * I'd essentially have very similar test cases here as I do in the service test.
+	 */
+
+	@Test
+	public void test() {
+		// TODO
+		// Assert.fail("not done");
+	}
+
+}
